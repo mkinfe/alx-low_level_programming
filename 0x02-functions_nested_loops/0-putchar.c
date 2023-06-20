64246d16864b4c954f6474b7e41e699c3db7f3e6
mkinfe <mkinfe818@gmail.com>
2023-06-20 04:40:41 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "0-putchar.c"
+
+/**
+* main - Print the word "_putchar"
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+		{
+		_putchar(p[i]);
+		}
+		_putchar('\n');
+	return (0);
+}

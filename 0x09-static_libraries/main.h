790390fbff8fb20e774a4fe45ff692c1d1b06fdf
mkinfe <mkinfe818@gmail.com>
2023-07-07 12:40:31 -0700

    stat lib

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <stdio.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,28 +21,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 

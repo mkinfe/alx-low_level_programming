050cedcdf9eea990e6a490b6a1a25369a7dc059a
mkinfe <mkinfe818@gmail.com>
2023-10-07 11:36:41 -0700

    creat hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,27 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_create - create a hash array of size size
- * @size: size of hash array
- * Return: pointer to hash table, NULL if failed
- */
+ * hash_table_create - create a hash table
+ * @size: size of the table
+ * Return: pointer to the table
+ **/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
-	hash_node_t *nodes;
+	hash_table_t *ht;
 
-	table = malloc(sizeof(hash_table_t));
-	if (!table)
+	if (size == 0)
+	return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
-	nodes = malloc(size * sizeof(hash_node_t));
-	if (!nodes)
+
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (ht->array == NULL)
 	{
-		free(table);
+		free(ht);
 		return (NULL);
 	}
-
-	table->size = size;
-	table->array = &nodes;
-
-	return (table);
+	return (ht);
 }
 
